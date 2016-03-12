80e059ad44b426ddcae9217b89878adfa6f6a887
exec <exec@mabinoger.de>
2016-03-12 12:29:42 +0100

    Added Login scene

---
+++ Assets/Aurora/Scripts/LoginForm.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Aura development team - Licensed under GNU GPL
+// For more information, see license file in the main folder
+
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LoginForm : MonoBehaviour
+{
+	public InputField TxtUsername;
+	public InputField TxtPassword;
+
+	public void BtnLogin_OnClick()
+	{
+		Debug.Log("BtnLogin_OnClick");
+		Debug.Log("Username: " + TxtUsername.text);
+		Debug.Log("Password: " + TxtPassword.text);
+	}
+}
