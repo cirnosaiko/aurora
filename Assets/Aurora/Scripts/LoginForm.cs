c9cad220d9b68f1c866c052a353d88a0d41bbbb0
exec <exec@mabinoger.de>
2016-03-13 20:07:00 +0100

    Improved MsgBox

--- Assets/Aurora/Scripts/LoginForm.cs
+++ Assets/Aurora/Scripts/LoginForm.cs
@@ -4,16 +4,35 @@
 using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using System;
 
 public class LoginForm : MonoBehaviour
 {
 	public InputField TxtUsername;
 	public InputField TxtPassword;
 
+	private bool connecting;
+	private float connectionTimeout;
+	private MsgBox processInfo;
+
+	void Update()
+	{
+		if (connecting && connectionTimeout <= Time.time)
+		{
+			connecting = false;
+			processInfo.Close();
+			MsgBox.Show("Connection failed.");
+		}
+	}
+
 	public void BtnLogin_OnClick()
 	{
 		Debug.Log("BtnLogin_OnClick");
 		Debug.Log("Username: " + TxtUsername.text);
 		Debug.Log("Password: " + TxtPassword.text);
+
+		connecting = true;
+		connectionTimeout = Time.time + 2;
+		processInfo = MsgBox.Show("Connecting...", MsgBoxButtons.None);
 	}
 }
--- Assets/Aurora/UI/MsgBox.cs
+++ Assets/Aurora/UI/MsgBox.cs
@@ -7,29 +7,45 @@ public class MsgBox : MonoBehaviour, IBeginDragHandler, IDragHandler
 {
 	private Vector2 dragStart;
 
-	public static void Show(string text)
+	public static MsgBox Show(string text)
+	{
+		return Show(text, MsgBoxButtons.Okay);
+	}
+
+	public static MsgBox Show(string text, MsgBoxButtons buttons)
 	{
 		var canvas = GameObject.Find("Canvas");
 		if (canvas == null)
 		{
 			Debug.LogError("MsgBox: Canvas not found.");
-			return;
+			return null;
 		}
 
 		var obj = Instantiate(Resources.Load("MsgBox")) as GameObject;
 
 		var transform = obj.transform as RectTransform;
-		var txtMessage = obj.transform.FindChild("TxtMessage").GetComponent<Text>();
+		var txtMessage = transform.FindChild("TxtMessage").GetComponent<Text>();
+		var btnOkay = transform.FindChild("BtnOkay").GetComponent<Button>();
+
+		if ((buttons & MsgBoxButtons.Okay) == 0)
+			btnOkay.gameObject.SetActive(false);
 
 		transform.SetParent(canvas.transform);
 		transform.position = new Vector3(Screen.width / 2, Screen.height / 2);
 
 		txtMessage.text = text;
+
+		return obj.GetComponent<MsgBox>();
+	}
+
+	public void Close()
+	{
+		Destroy(gameObject);
 	}
 
 	public void BtnOkay_OnClick()
 	{
-		Destroy(gameObject);
+		Close();
 	}
 
 	public void OnBeginDrag(PointerEventData eventData)
@@ -41,4 +57,10 @@ public class MsgBox : MonoBehaviour, IBeginDragHandler, IDragHandler
 	{
 		transform.position = dragStart + (eventData.position - eventData.pressPosition);
 	}
+}
+
+public enum MsgBoxButtons
+{
+	None = 0,
+	Okay = 1,
 }
