f711243bad520eb9fee9ecaf04f5c3ba038309f7
exec <exec@mabinoger.de>
2016-03-14 23:02:26 +0100

    Added character select scene

---
+++ Assets/Aurora/Scripts/CharacterSelectList.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Aura development team - Licensed under GNU GPL
+// For more information, see license file in the main folder
+
+using UnityEngine;
+using System.Collections;
+using System.Linq;
+using UnityEngine.UI;
+
+public class CharacterSelectList : MonoBehaviour
+{
+	public GameObject ButtonReference;
+
+	void Start()
+	{
+	}
+}
---
+++ Assets/Aurora/Scripts/ToLoginButton.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Aura development team - Licensed under GNU GPL
+// For more information, see license file in the main folder
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToLoginButton : MonoBehaviour
+{
+	public void OnClick()
+	{
+		Connection.Reset();
+		SceneManager.LoadScene("Login");
+	}
+}
