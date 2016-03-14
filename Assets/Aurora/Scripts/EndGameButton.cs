14bebedb399a17e377ecbe27ec019c1f467d9361
exec <exec@mabinoger.de>
2016-03-14 23:10:37 +0100

    Added End Game button to Login

---
+++ Assets/Aurora/Scripts/EndGameButton.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Aura development team - Licensed under GNU GPL
+// For more information, see license file in the main folder
+
+using UnityEngine;
+
+public class EndGameButton : MonoBehaviour
+{
+	public void OnClick()
+	{
+		Application.Quit();
+	}
+}
