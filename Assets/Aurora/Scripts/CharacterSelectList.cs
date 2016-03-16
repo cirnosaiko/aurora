50622b40f84a49de257d812fa9bcea47b782f96e
exec <exec@mabinoger.de>
2016-03-16 03:13:49 +0100

    Added channel selection to character selection

--- Assets/Aurora/Scripts/CharacterSelectList.cs
+++ Assets/Aurora/Scripts/CharacterSelectList.cs
@@ -6,17 +6,23 @@ using System.Collections;
 using System.Linq;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+using Aura.Mabi.Const;
 
 public class CharacterSelectList : MonoBehaviour
 {
+	public Transform ButtonsParent;
 	public GameObject ButtonReference;
+	public Dropdown SelChannel;
+	public Button BtnStart;
 
 	private CharacterInfo[] characters;
+	private List<ChannelInfo> channels = new List<ChannelInfo>();
+	private CharacterInfo selectedCharacter;
+	private ChannelInfo selectedChannel;
 
 	void Start()
 	{
-		var transform = this.transform;
-
 		characters = Connection.Characters.OrderBy(a => a.EntityId).ToArray();
 		foreach (var character in characters)
 		{
@@ -24,7 +30,7 @@ public class CharacterSelectList : MonoBehaviour
 			var name = string.Format("{0} ({1})", character.Name, character.Server);
 
 			var buttonObj = GameObject.Instantiate(ButtonReference);
-			buttonObj.transform.SetParent(transform);
+			buttonObj.transform.SetParent(ButtonsParent);
 
 			var text = buttonObj.GetComponentInChildren<Text>();
 			text.text = name;
@@ -35,23 +41,62 @@ public class CharacterSelectList : MonoBehaviour
 			var button = buttonObj.GetComponent<Button>();
 			button.onClick.AddListener(() => { OnCharacterSelected(entityId); });
 		}
+
+		BtnStart.interactable = false;
+		BtnStart.onClick.AddListener(BtnStart_OnClick);
+
+		SelChannel.interactable = false;
+		SelChannel.onValueChanged.AddListener(SelChannel_OnValueChanged);
+		SelChannel.ClearOptions();
 	}
 
 	private void OnCharacterSelected(long entityId)
 	{
-		var character = characters.FirstOrDefault(a => a.EntityId == entityId);
-		if (character == null)
+		selectedCharacter = characters.FirstOrDefault(a => a.EntityId == entityId);
+		if (selectedCharacter == null)
 		{
 			Debug.LogErrorFormat("Missing character '{0}'.", entityId);
 			return;
 		}
 
-		if (character.IsPartner)
+		UpdateChannels(selectedCharacter.Server);
+		SelChannel.interactable = true;
+	}
+
+	private void UpdateChannels(string serverName)
+	{
+		SelChannel.ClearOptions();
+		channels.Clear();
+
+		var server = Connection.Servers.FirstOrDefault(a => a.Name == serverName);
+		if (server == null)
+			return;
+
+		channels.AddRange(server.Channels.OrderBy(a => a.Name));
+
+		var list = new List<string>();
+		foreach (var channel in channels)
+			list.Add(channel.Name + " [" + channel.State + "]");
+
+		SelChannel.AddOptions(list);
+		SelChannel_OnValueChanged(0);
+	}
+
+	private void SelChannel_OnValueChanged(int selectedIndex)
+	{
+		selectedChannel = channels[selectedIndex];
+		BtnStart.interactable = (selectedChannel.State != ChannelState.Maintenance && selectedChannel.State < ChannelState.Bursting && !selectedCharacter.IsPartner);
+	}
+
+	private void BtnStart_OnClick()
+	{
+		if (selectedCharacter == null || selectedChannel == null)
 		{
-			MsgBox.Show("You can't use a Partner to login.");
+			Debug.LogError("Selected character or channel is null.");
 			return;
 		}
 
-		MsgBox.Show("TODO: Login with " + character.Name + ".");
+		MsgBox.Show("TODO: Login with " + selectedCharacter.Name + " on " + selectedChannel.Name + ".");
+		//SceneManager.LoadScene("Uladh_main");
 	}
 }
