5bcb2f4e58f8e1aea5b9ada7892fb19990d13205
exec <exec@mabinoger.de>
2016-03-26 13:20:10 +0100

    Added system dummy for offline testing

---
+++ Assets/Aurora/Scripts/System/AddDummyEntity.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AddDummyEntity : MonoBehaviour
+{
+	public GameObject Dummy;
+
+	void Start()
+	{
+		var parentObj = GameObject.Find("Entities");
+		var terrainObj = GameObject.Find("Terrain");
+
+		if (parentObj != null && terrainObj != null)
+		{
+			var terrain = terrainObj.GetComponent<Terrain>();
+			var x = terrain.terrainData.size.x / 2;
+			var z = terrain.terrainData.size.z / 2;
+
+			var obj = (GameObject)GameObject.Instantiate(Dummy, new Vector3(x, 0, z), Quaternion.identity);
+			obj.transform.SetParent(parentObj.transform);
+		}
+	}
+}
